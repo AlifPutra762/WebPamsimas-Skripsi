20b223b2fbb785a94373aa2226c08581aa63c28f
FriedRice <hello@FriedRice.com>
2025-09-25 14:54:18 +0700

    p

--- resources/views/layout/sbadmin.blade.php
+++ resources/views/layout/sbadmin.blade.php
@@ -28,8 +28,8 @@
         integrity="sha256-9kPW/n5nn53j4WMRYAxe9c1rCY96Oogo/MKSVdKzPmI=" crossorigin="anonymous" />
     <!--end::Third Party Plugin(Bootstrap Icons)-->
     <!--begin::Required Plugin(AdminLTE)-->
-    //
-    <link rel="stylesheet" href="{{ asset('templates/dist/css/adminlte.css') }}" />
+
+    <!--<link rel="stylesheet" href="{{ asset('templates/dist/css/adminlte.css') }}" />-->
     <!--end::Required Plugin(AdminLTE)-->
 
     <!-- Select2 CSS -->
@@ -182,8 +182,8 @@
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
     </script>
     <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
-    //
-    <script src="{{ asset('templates/dist/js/adminlte.js') }}"></script>
+
+    <!--<script src="{{ asset('templates/dist/js/adminlte.js') }}"></script>-->
     <!--end::Required Plugin(AdminLTE)--><!--begin::OverlayScrollbars Configure-->
     <script>
         const SELECTOR_SIDEBAR_WRAPPER = '.sidebar-wrapper';
